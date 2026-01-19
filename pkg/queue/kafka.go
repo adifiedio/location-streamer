8e0fbf23c3f58b44c27931b9eed7a9cf187c4944
adarsh <adarshagarwal1292@gmail.com>
2026-01-19 13:00:39 +0530

    fix: fixed kafka bugs

--- pkg/queue/kafka.go
+++ pkg/queue/kafka.go
@@ -14,9 +14,12 @@ type Producer struct {
 
 func NewProducer(brokers []string, topic string) *Producer {
 	writer := &kafka.Writer{
-		Addr:     kafka.TCP(brokers...),
-		Topic:    topic,
-		Balancer: &kafka.LeastBytes{},
+		Addr:                   kafka.TCP(brokers...),
+		Topic:                  topic,
+		Balancer:               &kafka.LeastBytes{},
+		RequiredAcks:           kafka.RequireOne, // ensure it's actually written
+		AllowAutoTopicCreation: true,
+		Async:                  false, // block until written
 		// add some logging for troubleshooting
 		ErrorLogger: kafka.LoggerFunc(func(s string, i ...interface{}) {
 			log.Printf("kafka producer error: "+s, i...)
@@ -47,11 +50,12 @@ type Consumer struct {
 
 func NewConsumer(brokers []string, topic string, groupID string) *Consumer {
 	reader := kafka.NewReader(kafka.ReaderConfig{
-		Brokers:  brokers,
-		Topic:    topic,
-		GroupID:  groupID,
-		MinBytes: 1,    // 1 byte - process as soon as we have any data
-		MaxBytes: 10e6, // 10MB
+		Brokers:     brokers,
+		Topic:       topic,
+		GroupID:     groupID,
+		MinBytes:    1,    // 1 byte - process as soon as we have any data
+		MaxBytes:    10e6, // 10MB
+		StartOffset: kafka.FirstOffset,
 	})
 	return &Consumer{reader: reader}
 }
