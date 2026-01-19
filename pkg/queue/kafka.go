c1c60464b563c6060c9f84fafd9d850cb4bb8f3b
adarsh <adarshagarwal1292@gmail.com>
2026-01-19 14:09:24 +0530

    fix: fixed kafka error

--- pkg/queue/kafka.go
+++ pkg/queue/kafka.go
@@ -4,15 +4,56 @@ import (
 	"context"
 	"encoding/json"
 	"log"
+	"net"
+	"strconv"
 
 	"github.com/segmentio/kafka-go"
 )
+
+// EnsureTopic checks if a topic exists, if not it creates it.
+func EnsureTopic(broker string, topic string) error {
+	conn, err := kafka.Dial("tcp", broker)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	controller, err := conn.Controller()
+	if err != nil {
+		return err
+	}
+	var controllerConn *kafka.Conn
+	controllerConn, err = kafka.Dial("tcp", net.JoinHostPort(controller.Host, strconv.Itoa(controller.Port)))
+	if err != nil {
+		return err
+	}
+	defer controllerConn.Close()
+
+	topicConfigs := []kafka.TopicConfig{
+		{
+			Topic:             topic,
+			NumPartitions:     1,
+			ReplicationFactor: 1,
+		},
+	}
+
+	err = controllerConn.CreateTopics(topicConfigs...)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 type Producer struct {
 	writer *kafka.Writer
 }
 
 func NewProducer(brokers []string, topic string) *Producer {
+	// auto create topic on first use in dev/local
+	if len(brokers) > 0 {
+		_ = EnsureTopic(brokers[0], topic)
+	}
+
 	writer := &kafka.Writer{
 		Addr:                   kafka.TCP(brokers...),
 		Topic:                  topic,
@@ -49,6 +90,11 @@ type Consumer struct {
 }
 
 func NewConsumer(brokers []string, topic string, groupID string) *Consumer {
+	// auto create topic on first use in dev/local
+	if len(brokers) > 0 {
+		_ = EnsureTopic(brokers[0], topic)
+	}
+
 	reader := kafka.NewReader(kafka.ReaderConfig{
 		Brokers:     brokers,
 		Topic:       topic,
@@ -56,6 +102,13 @@ func NewConsumer(brokers []string, topic string, groupID string) *Consumer {
 		MinBytes:    1,    // 1 byte - process as soon as we have any data
 		MaxBytes:    10e6, // 10MB
 		StartOffset: kafka.FirstOffset,
+		// add loggers for debugging
+		Logger: kafka.LoggerFunc(func(s string, i ...interface{}) {
+			log.Printf("kafka consumer: "+s, i...)
+		}),
+		ErrorLogger: kafka.LoggerFunc(func(s string, i ...interface{}) {
+			log.Printf("kafka consumer error: "+s, i...)
+		}),
 	})
 	return &Consumer{reader: reader}
 }
